c24c8dafa51144018e10c026f37a011c605f2ff5
lrasata <rmliantsoa@gmail.com>
2022-02-27 18:17:52 +1300

    setting up surrency exchange service

---
+++ src/test/java/com/lrasata/microservices/currencyexchangemicroservice/CurrencyExchangeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrasata.microservices.currencyexchangemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
